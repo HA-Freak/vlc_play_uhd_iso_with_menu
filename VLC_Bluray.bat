d946f7c8a31bce5c0b0515ad91e36352fa8a9f1f
HA-Freak <111146692+HA-Freak@users.noreply.github.com>
2024-12-30 00:48:50 +0100

    Update VLC_Bluray.bat
    change to polyglot

--- VLC_Bluray.bat
+++ VLC_Bluray.bat
@@ -1,1 +1,15 @@
-powershell -command "try{$VlcPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\VideoLAN\VLC' -Name InstallDir -ErrorAction Stop}catch [System.Exception]{$VlcPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\VideoLAN\VLC' -Name InstallDir};$esc_file_path = [uri]::EscapeUriString('%~f1');$VlcExe = $VlcPath + '\vlc.exe';$args = 'bluray:///' + $esc_file_path;Start-Process -FilePath $VlcExe -ArgumentList $args;"
+<# : B
+@echo off && setlocal && cd %~dp0
+powershell -nol -nop -ex bypass -c "iex $($PSScriptName='%~nx0';$file='%~f1';type '%~dpf0' -raw)"
+endlocal && goto:eof
+#>
+$PSScriptRoot = (gl).Path
+try{
+	$VlcPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\VideoLAN\VLC' -Name InstallDir -ErrorAction Stop
+}catch [System.Exception]{
+	$VlcPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\VideoLAN\VLC' -Name InstallDir
+}
+$esc_file_path = [uri]::EscapeUriString($file)
+$VlcExe = $VlcPath + '\vlc.exe'
+$args = 'bluray:///' + $esc_file_path
+Start-Process -FilePath $VlcExe -ArgumentList $args
